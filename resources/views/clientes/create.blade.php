ff8a15d4f6a78de667dee650c380264483e1f3e3
HERNANMAMANI <hernanmamani77892673@gmail.com>
2021-09-01 18:26:20 -0400

    first commit

---
+++ resources/views/clientes/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+@section('title','Crear Cliente')
+@section('content')
+<h1>En esta seccion podras agregar un cliente</h1>
+    
+@endsection
---
+++ resources/views/clientes/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+@section('title','Cliente'.$cliente)
+@section('content')
+    <h1>{{$cliente}} bienvenido a la seccion cliente</h1>
+    
+@endsection
